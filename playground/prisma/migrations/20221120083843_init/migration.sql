6b8dffde5195fc7c9d5dbf9789c6c6268148fd27
wobsoriano <sorianorobertc@gmail.com>
2022-11-20 00:52:18 -0800

    add prisma files to ignored

--- playground/prisma/migrations/20221120083843_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "title" TEXT NOT NULL,
-    "completed" BOOLEAN NOT NULL,
-    "content" TEXT
-);
