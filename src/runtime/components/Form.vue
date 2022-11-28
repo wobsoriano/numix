4035e8e42d2a43e3a303a504d5a53f049aba3560
wobsoriano <sorianorobertc@gmail.com>
2022-11-28 09:13:32 -0800

    fix Form types

---
+++ src/runtime/components/Form.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import FormImpl from './FormImpl'
+
+withDefaults(defineProps<{
+  replace?: boolean
+  onSubmit?: Function
+}>(), {
+  replace: false,
+})
+</script>
+
+<template>
+  <FormImpl :replace="replace" :on-submit="onSubmit">
+    <slot />
+  </FormImpl>
+</template>
